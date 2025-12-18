a676555e363916ccf23f1586eb219acf70920e30
adarshkumar-wd <adarshff06@gmail.com>
2025-12-18 16:15:56 +0000

    changes from server

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/test-from-server', function () {
+    return 'Hello from Hostinger server';
+});
